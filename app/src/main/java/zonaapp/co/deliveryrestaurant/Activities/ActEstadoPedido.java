f28a631a3757dc1bb34fac40f732e444ff6aa3a6
servintesas <xp.sistem@hotmail.com>
2016-01-20 21:58:05 -0500

    pedidoijki

--- app/src/main/java/zonaapp/co/deliveryrestaurant/Activities/ActEstadoPedido.java
+++ app/src/main/java/zonaapp/co/deliveryrestaurant/Activities/ActEstadoPedido.java
@@ -3,7 +3,6 @@ package zonaapp.co.deliveryrestaurant.Activities;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -19,8 +18,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.baoyz.swipemenulistview.SwipeMenuListView;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.Gson;
 
@@ -119,7 +116,7 @@ public class ActEstadoPedido extends AppCompatActivity {
             }
         } else {
             alertDialog.dismiss();
-            Toast.makeText(ActEstadoPedido.this, "Problemas al recuperar la información", Toast.LENGTH_LONG).show();
+            Toast.makeText(ActEstadoPedido.this,"Problemas al recuperar la información", Toast.LENGTH_LONG).show();
         }
 
         return false;
@@ -134,44 +131,4 @@ public class ActEstadoPedido extends AppCompatActivity {
         super.onBackPressed();  // optional depending on your needs
     }
 
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "ActEstadoPedido Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://zonaapp.co.deliveryrestaurant.Activities/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "ActEstadoPedido Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://zonaapp.co.deliveryrestaurant.Activities/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
